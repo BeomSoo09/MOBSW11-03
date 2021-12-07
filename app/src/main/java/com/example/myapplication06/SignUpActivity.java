9c1d15a2fdd120729d97a1359566d657f000f4fc
ahn0715 <anbbum@gmail.com>
2021-12-08 00:57:55 +0900

    로그인 및 DB 연동

--- app/src/main/java/com/example/myapplication06/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication06/SignUpActivity.java
@@ -1,21 +1,17 @@
 package com.example.myapplication06;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
-    private static final String TAG = "SignUpActivity";
     private FirebaseAuth mAuth;
 
     @Override
@@ -27,44 +23,61 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.signUpButton).setOnClickListener(onClickListener);
+        findViewById(R.id.gotoLoginButton).setOnClickListener(onClickListener);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+    public void onBackPressed(){
+        super.onBackPressed();
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+    View.OnClickListener onClickListener = (v) ->  {
             switch (v.getId()){
                 case R.id.signUpButton:
                     signUp();
                     break;
+                case R.id.gotoLoginButton:
+                    myStartActivity(LoginActivity.class);
+                    break;
             }
-        }
     };
     private void signUp(){
         String email=((EditText)findViewById(R.id.emailEditText)).getText().toString();
         String password=((EditText)findViewById(R.id.passwordEditText)).getText().toString();
+        String passwordCheck=((EditText)findViewById(R.id.passwordCheckEditText)).getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //UI
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            //UI
-                        }
-                    }
-                });
+        if(email.length()>0 && password.length()>0 && passwordCheck.length()>0){
+            if(password.equals(passwordCheck)){
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, (task) ->  {
+                                if (task.isSuccessful()) {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startToast("회원가입이 성공했습니다.");
+                                    myStartActivity(MainActivity.class);
+                                    //UI
+                                } else {
+                                    if(task.getException() != null){
+                                        startToast(task.getException().toString());
+                                    }
+                                }
+                        });
+            }else{
+                startToast("비밀번호가 일치하지 않습니다.");
+            }
+        }else{
+            startToast("이메일 또는 비밀번호를 입력해 주세요.");
+        }
+    }
+    private void startToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    private void myStartActivity(Class c){
+        Intent intent = new Intent(this,c);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
